831570dcdbfa9244bae6d34ca70f4eced02cd6a9
flying981 <3176643279@qq.com>
2025-12-10 13:11:07 +0800

    1.4，合并

--- src/test.java
+++ src/test.java
@@ -1,3 +1,4 @@
 
 System.out.print("1.1的分支");
 System.out.print("1.3的分支---在远程仓库修改");
+System.out.print("1.4的分支----学习合并，多个文件");
--- src/test1.java
+++ src/test1.java
@@ -2,3 +2,4 @@
 System.out.print("1.1的分支");
 System.out.print("1.3的分支----学习合并");
 System.out.print("1.2的分支----学习合并");
+System.out.print("1.4的分支----学习合并，多个文件");
